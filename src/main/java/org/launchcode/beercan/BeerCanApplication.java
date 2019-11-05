8b55852953aa36bc7f87963cedb2136901661d0c
Michael Hacker <hack1973@swbell.net>
2019-11-05 12:06:39 -0600

    initial setup

---
+++ src/main/java/org/launchcode/beercan/BeerCanApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.beercan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerCanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerCanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/beercan/BeerCanApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.beercan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerCanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
